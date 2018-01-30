53c7f7daf7634ad5a2140daa839c89ee83be5347
Rodrigo Bistolfi <rbistolfi@gmail.com>
2018-01-30 10:08:43 -0300

    script for building the parser

---
+++ build_parser.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+export CLASSPATH=".:/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH"
+alias antlr4='java -jar /usr/local/lib/antlr-4.7.1-complete.jar'       
+antlr4 PS.g4 -o latex2sympy/gen                                    
